966e4da823ea539a1e8866c89f8291489ba45459
Kombiah <kombiahrk@yahoo.com>
2024-04-30 19:59:46 +0530

    Declare a new state variable as "value"

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
 export const TodoForm = () => {
+  //Declare a new state variable, which we'll call "value"
+  const [value, setValue] = React.useState('Hai');
+
   return (
     <form className='TodoForm'>
-      <input type='text' className='todo-input' placeholder='What is the task today?'/>
+      <input type='text' className='todo-input' value={value} placeholder='What is the task today?'/>
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
