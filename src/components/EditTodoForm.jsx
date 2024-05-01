fee030c36dc3c0065b4d34d98f98f1f2e12a7d71
Kombiah <kombiahrk@yahoo.com>
2024-05-01 12:49:34 +0530

    EditTodoForm Component Updated based on TodoForm

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
-export const EditTodoForm = () => {
+export const EditTodoForm = ({editTodo, task }) => {
+  const [value, setValue] = React.useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    editTodo(value, task.id);
+  }
   return (
-    <div>EditTodoForm</div>
+    <form className='TodoForm' onSubmit={handleSubmit}>
+      <input type='text' className='todo-input' value={value} placeholder='Update task' onChange={(e) => setValue(e.target.value)} />
+      <button type='submit' className='todo-btn'>Update Task</button>
+    </form>
   )
 }
