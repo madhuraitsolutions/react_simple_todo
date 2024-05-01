02d0bb1f6a871ce4b46bc87a99888ae60828ed80
Kombiah <kombiahrk@yahoo.com>
2024-05-01 11:05:44 +0530

    Now used the setValue method to update the value in the change event

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -8,8 +8,9 @@ export const TodoForm = () => {
 
   return (
     <form className='TodoForm'>
-      {/* <input type='text' className='todo-input' value={value} placeholder='What is the task today?'/> */}
-      <input type='text' className='todo-input' value={value} placeholder='What is the task today?' onChange={(e) => console.log(e.target.value)}/>
+      {/*1 <input type='text' className='todo-input' value={value} placeholder='What is the task today?'/> */}
+      {/*2 <input type='text' className='todo-input' value={value} placeholder='What is the task today?' onChange={(e) => console.log(e.target.value)}/> */}
+      <input type='text' className='todo-input' value={value} placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)}/>
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
