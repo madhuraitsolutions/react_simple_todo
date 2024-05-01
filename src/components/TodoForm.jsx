0dfc30046825801e355041f7e5e5281bd4e0f256
Kombiah <kombiahrk@yahoo.com>
2024-05-01 11:17:06 +0530

    While submitting a form, capture the event using onSubmit and print the form values to the console using an arrow function named handleSubmit.

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 export const TodoForm = () => {
   //Declare a new state variable, which we'll call "value"
   // const [value, setValue] = React.useState('Hai'); 
-  const [value, setValue] = React.useState(''); 
+  const [value, setValue] = React.useState('');
 
-
+  const handleSubmit = (e) => {
+    e.preventDefault(); // prevent page refresh
+    console.log(value);
+  }
   return (
-    <form className='TodoForm'>
+    <form className='TodoForm' onSubmit={handleSubmit}>
       {/*1 <input type='text' className='todo-input' value={value} placeholder='What is the task today?'/> */}
       {/*2 <input type='text' className='todo-input' value={value} placeholder='What is the task today?' onChange={(e) => console.log(e.target.value)}/> */}
-      <input type='text' className='todo-input' value={value} placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)}/>
+      <input type='text' className='todo-input' value={value} placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)} />
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
