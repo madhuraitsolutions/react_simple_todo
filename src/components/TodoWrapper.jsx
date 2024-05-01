2839d8596713ee10d1a0b9dda5d6b4338f8ce66b
Kombiah <kombiahrk@yahoo.com>
2024-05-01 11:38:09 +0530

    Created addTask arrow function to setValues of todos

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
+import { v4 as uuidv4 } from 'uuid'
 
 export const TodoWrapper = () => {
+  const [todos, setTodos] = useState([]); // initial value is an empty array
+  const addTask = (todo) => {
+    setTodos([
+      ...todos, // spread operator
+      {
+        id : uuidv4(), // generate a unique id
+        task : todo, // task is the value passed from TodoForm
+        completed: false, // initial value is false
+        isEditing: false // initial value is false
+      }
+    ])
+    console.log(todos) //
+  }
   return (
     <div className='TodoWrapper'>
         <TodoForm addTodo={addTask}/> 
