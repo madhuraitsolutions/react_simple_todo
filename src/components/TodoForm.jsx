afadfe038b2bc4071b467db5a214a0ef4b53c65d
Kombiah <kombiahrk@yahoo.com>
2024-05-01 11:03:32 +0530

    Removed default value and console the output onChange event

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 
 export const TodoForm = () => {
   //Declare a new state variable, which we'll call "value"
-  const [value, setValue] = React.useState('Hai'); 
+  // const [value, setValue] = React.useState('Hai'); 
+  const [value, setValue] = React.useState(''); 
+
 
   return (
     <form className='TodoForm'>
-      <input type='text' className='todo-input' value={value} placeholder='What is the task today?'/>
+      {/* <input type='text' className='todo-input' value={value} placeholder='What is the task today?'/> */}
+      <input type='text' className='todo-input' value={value} placeholder='What is the task today?' onChange={(e) => console.log(e.target.value)}/>
       <button type='submit' className='todo-btn'>Add Task</button>
     </form>
   )
