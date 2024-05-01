c7e7804d2cc78773f0ced16e8cedad3926f0ea04
Kombiah <kombiahrk@yahoo.com>
2024-05-01 11:42:33 +0530

    Import and Use the Todo Component in TodoWrapperComponent

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { TodoForm } from './TodoForm'
 import { v4 as uuidv4 } from 'uuid'
+import { Todo } from './Todo';
 
 export const TodoWrapper = () => {
   const [todos, setTodos] = useState([]); // initial value is an empty array
@@ -19,6 +20,7 @@ export const TodoWrapper = () => {
   return (
     <div className='TodoWrapper'>
         <TodoForm addTodo={addTask}/> 
+        <Todo />
     </div>
   )
 }
