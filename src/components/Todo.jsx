27808bdd70930497a47f4cce3dc8f2f5c221e39b
Kombiah <kombiahrk@yahoo.com>
2024-05-01 12:34:22 +0530

    Added deleteTodo Props to get the value of the id of the particular task and pass to the TodoWrapperComponent

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,13 @@ import { faPenToSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
-export const Todo = ({todo, toggleComplete}) => {
+export const Todo = ({todo, toggleComplete, deleteTodo}) => {
   return (
     <div className='Todo'>
       <p className={`${todo.completed ? 'completed' : 'incompleted'}`} onClick={() => toggleComplete(todo.id)}>{todo.task}</p>
       <div>
         <FontAwesomeIcon icon={faPenToSquare} />
-        <FontAwesomeIcon icon={faTrash} />
+        <FontAwesomeIcon icon={faTrash} onClick={() => deleteTodo(todo.id)} />
       </div>
     </div>
   )
